188ab8898d5b434d70c25f558ecf08e423126171
i340347 <luuk.hermans@student.fontys.nl>
2016-09-26 19:00:39 +0200

    setScene methode toegevoegd.

---
+++ Soccar [Client]/src/nl/soccar/ui/MainMenuFXMLController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.soccar.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import nl.soccar.library.Soccar;
+
+/**
+ * FXML Controller class
+ *
+ * @author Luuk
+ */
+public class MainMenuFXMLController implements Initializable {
+
+    @FXML
+    Button btnCreateRoom;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        btnCreateRoom.setOnAction(e -> {
+            Main.getInstance().setScene("CreateRoomFXML.fxml");
+        });
+    }
+    
+}
