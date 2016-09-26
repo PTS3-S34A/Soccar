2baae3088b1880ff880e2546739b0a65569fd4c0
i340347 <luuk.hermans@student.fontys.nl>
2016-09-26 12:16:28 +0200

    Button eventhandlers added.

--- Soccar [Client]/src/nl/soccar/ui/LoginFXMLController.java
+++ Soccar [Client]/src/nl/soccar/ui/LoginFXMLController.java
@@ -2,10 +2,14 @@ package nl.soccar.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -13,21 +17,30 @@ import javafx.scene.control.TextField;
  * @author PTS34A
  */
 public class LoginFXMLController implements Initializable {
-  
-@FXML
-private Button btnSelectSportscar;
-@FXML
-private Button btnSelectPickup;
-@FXML
-private Button btnSelectCasual;
-@FXML
-private Button btnLogin;
-@FXML
-private TextField txtfieldName;
-        
+
+    @FXML
+    private Button btnSelectSportscar;
+    @FXML
+    private Button btnSelectPickup;
+    @FXML
+    private Button btnSelectCasual;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private TextField txtfieldName;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btnLogin.setOnAction(e -> {
+            login();
+        });
     }
-    
+
+    public void login() {
+        if (!txtfieldName.getText().isEmpty()) {
+            Main.getInstance().login(txtfieldName.getText());
+        } else {
+            txtfieldName.setStyle("-fx-text-box-border: red ; -fx-focus-color: red ;");
+        }
+    }
 }
