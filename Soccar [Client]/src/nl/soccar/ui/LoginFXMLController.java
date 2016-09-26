389c8b69631da38e57f618cb4ad68209211a50b1
i340347 <luuk.hermans@student.fontys.nl>
2016-09-26 10:56:56 +0200

    Main login setup.

--- Soccar [Client]/src/nl/soccar/ui/LoginFXMLController.java
+++ Soccar [Client]/src/nl/soccar/ui/LoginFXMLController.java
@@ -2,7 +2,10 @@ package nl.soccar.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -10,7 +13,18 @@ import javafx.fxml.Initializable;
  * @author PTS34A
  */
 public class LoginFXMLController implements Initializable {
-
+  
+@FXML
+private Button btnSelectSportscar;
+@FXML
+private Button btnSelectPickup;
+@FXML
+private Button btnSelectCasual;
+@FXML
+private Button btnLogin;
+@FXML
+private TextField txtfieldName;
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
