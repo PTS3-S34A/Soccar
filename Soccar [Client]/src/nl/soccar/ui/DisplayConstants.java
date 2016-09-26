90ea66ff564186e885367839437a299fe8d4b528
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-09-26 14:51:01 +0200

    Added display constants

---
+++ Soccar [Client]/src/nl/soccar/ui/DisplayConstants.java
@@ -0,0 +1,14 @@
+package nl.soccar.ui;
+
+/**
+ *
+ * @author PTS34A
+ */
+public final class DisplayConstants {
+    
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 800;
+    
+    private DisplayConstants() {
+    }
+}
