6376b5a260a3d1256864e4eb867a2082bf558f82
i340347 <luuk.hermans@student.fontys.nl>
2016-09-27 09:37:52 +0200

    Added constants & re-used setScene method.

---
+++ Soccar [Client]/src/nl/soccar/ui/fx/FXMLConstants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.soccar.ui.fx;
+
+/**
+ *
+ * @author Luuk
+ */
+public final class FXMLConstants {
+    
+    public static final String LOCATION_MAIN_MENU = "resources/fxml/MainMenuFXML.fxml";
+    public static final String LOCATION_LOGIN = "resources/fxml/LoginFXML.fxml";
+    public static final String LOCATION_CREATE_ROOM = "resources/fxml/CreateRoomFXML.fxml";
+    
+    private FXMLConstants() {
+    }
+    
+}
