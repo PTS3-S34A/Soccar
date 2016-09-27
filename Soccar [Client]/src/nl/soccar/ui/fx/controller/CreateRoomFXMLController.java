be13ba540ee8e5d62e197e57a3e6ccc17ca1f597
i340347 <luuk.hermans@student.fontys.nl>
2016-09-27 11:24:50 +0200

    login/ logOut methods, UI design.

--- Soccar [Client]/src/nl/soccar/ui/fx/controller/CreateRoomFXMLController.java
+++ Soccar [Client]/src/nl/soccar/ui/fx/controller/CreateRoomFXMLController.java
@@ -7,7 +7,12 @@ package nl.soccar.ui.fx.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import nl.soccar.library.Soccar;
+import nl.soccar.ui.Main;
 
 /**
  * FXML Controller class
@@ -15,13 +20,22 @@ import javafx.fxml.Initializable;
  * @author Luuk
  */
 public class CreateRoomFXMLController implements Initializable {
-
+    
+    @FXML
+    private Button btnLogOut;
+    @FXML
+    private Label lblUsername;
     /**
      * Initializes the controller class.
      */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btnLogOut.setOnAction(e -> {
+            Main.getInstance().logOut();
+        });
+        
+        lblUsername.setText(Soccar.getInstance().getCurrentPlayer().getUsername()); 
     }    
     
 }
