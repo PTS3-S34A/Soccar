46cb54d9de9fc02e3e582eaee2ecd6cdd123b2ba
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-10-03 11:03:13 +0200

    Documentation added to input classes.

--- Soccar [Client]/src/nl/soccar/ui/input/Keyboard.java
+++ Soccar [Client]/src/nl/soccar/ui/input/Keyboard.java
@@ -12,6 +12,11 @@ public final class Keyboard {
 
     private static final Keyboard instance = new Keyboard();
 
+    /**
+     * Method that gets this Keyboard object.
+     * 
+     * @return This keyboard object.
+     */
     public static Keyboard getInstance() {
         return instance;
     }
@@ -22,16 +27,32 @@ public final class Keyboard {
         pressedKeys = new ArrayList<>();
     }
 
+    /**
+     * Method that checks if the given key is pressed.
+     * 
+     * @param code The keycode that needs to be checked.
+     * @return 
+     */
     public boolean isPressed(KeyCode code) {
         return pressedKeys.contains(code);
     }
 
+    /**
+     * Method that adds the given KeyCode to the pressedKeys list.
+     * 
+     * @param code The keycode that needs to be added to the pressedKeys list.
+     */
     public void setKeyPressed(KeyCode code) {
         if (!pressedKeys.contains(code)) {
             pressedKeys.add(code);
         }
     }
 
+    /**
+     * Method that removes the given KeyCode of the pressedKeys list.
+     * 
+     * @param code The keycode that needs to be removed of the pressedKeys list.
+     */
     public void setKeyReleased(KeyCode code) {
         pressedKeys.remove(code);
     }
