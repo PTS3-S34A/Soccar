1f8ec3dcbbad0aefa6f4abd494e76b11c016c70e
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2016-10-03 10:41:35 +0200

    Documentation added to Fx classes.

--- Soccar [Client]/src/nl/soccar/ui/fx/DrawableFx.java
+++ Soccar [Client]/src/nl/soccar/ui/fx/DrawableFx.java
@@ -11,15 +11,31 @@ public abstract class DrawableFx<T> implements Drawable {
     private GameCanvasFx canvas;
     private T model;
 
+    /**
+     * Initiates a new DrawableFx object.
+     * 
+     * @param canvas The drawable context of this DrawableFx.
+     * @param model  The used model of this DrawableFx.
+     */
     public DrawableFx(GameCanvasFx canvas, T model) {
         this.canvas = canvas;
         this.model = model;
     }
 
+    /**
+     * Method that returns the canvas of this DrawableFx.
+     * 
+     * @return The canvas of this DrawableFx.
+     */
     protected GameCanvasFx getCanvas() {
         return canvas;
     }
 
+    /**
+     * Method that returns the used model in this DrawableFx.
+     * 
+     * @return The model.
+     */
     @Override
     public final T getModel() {
         return model;
