b686e8f8d8ae0a79e5a2f5a335ee0ca24728118c
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-08 17:42:48 +0200

    Finished testing all nl.soccar.util classes. All tests are documented.

---
+++ Soccar [Client]/test/nl/soccar/util/test/PhysicsUtilitiesTest.java
@@ -0,0 +1,89 @@
+package nl.soccar.util.test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import nl.soccar.util.PhysicsUtilities;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit test that tests the nl.soccar.util PhysicsUtilities class.
+ *
+ * @author PTS34A
+ */
+public class PhysicsUtilitiesTest {
+
+    /**
+     * Tests the toPixelX method.
+     */
+    @Test
+    public void toPixelXTest() {
+        assertEquals(Math.round(100.0F), Math.round(PhysicsUtilities.toPixelX(10.0F)));
+    }
+
+    /**
+     * Tests the toPixelY method.
+     */
+    @Test
+    public void toPixelYTest() {
+        assertEquals(Math.round(700.0F), Math.round(PhysicsUtilities.toPixelY(10.0F)));
+    }
+
+    /**
+     * Tests the toPixelWidth method.
+     */
+    @Test
+    public void toPixelWidthTest() {
+        assertEquals(Math.round(100.0F), Math.round(PhysicsUtilities.toPixelWidth(10.0F)));
+    }
+
+    /**
+     * Tests the toPixelHeight method.
+     */
+    @Test
+    public void toPixelHeightTest() {
+        assertEquals(Math.round(100.0F), Math.round(PhysicsUtilities.toPixelHeight(10.0F)));
+    }
+
+    /**
+     * Tests the calculateCarHeight method.
+     */
+    @Test
+    public void calculateCarHeightTest() {
+        assertEquals(Math.round(24.0F), Math.round(PhysicsUtilities.calculateCarHeight(10.0F)));
+    }
+
+    /**
+     * Tests the calculateWheelWidth method.
+     */
+    @Test
+    public void calculateWheelWidthTest() {
+        assertEquals(Math.round(2.0F), Math.round(PhysicsUtilities.calculateWheelWidth(10.0F)));
+    }
+
+    /**
+     * Tests the calculateWheelHeight method.
+     */
+    @Test
+    public void calculateWheelHeightTest() {
+        assertEquals(Math.round(20.0F), Math.round(PhysicsUtilities.calculateWheelHeight(10.0F)));
+    }
+
+    /**
+     * Marks the private constructor of the PhysicsUtilities class as tested in JaCoCo.
+     *
+     * @throws NoSuchMethodException Does not apply.
+     * @throws IllegalAccessException Does not apply.
+     * @throws InvocationTargetException Does not apply.
+     * @throws InstantiationException Does not apply.
+     */
+    @Test
+    public void privateConstructorTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<PhysicsUtilities> constructor = PhysicsUtilities.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+}
