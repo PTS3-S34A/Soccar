d16fed3731058bd6cee76900cea5010fbbfa2ada
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-08 15:44:44 +0200

    Added a testclass that realizes 100% JaCoCo coverage for the nl.soccar.library.enumeration enumerations.

---
+++ Soccar [Client]/test/nl/soccar/library/enumerations/EnumerationTest.java
@@ -0,0 +1,35 @@
+package nl.soccar.library.enumerations;
+
+import nl.soccar.library.enumeration.BallType;
+import nl.soccar.library.enumeration.CarType;
+import nl.soccar.library.enumeration.Duration;
+import nl.soccar.library.enumeration.EventType;
+import nl.soccar.library.enumeration.GameStatus;
+import nl.soccar.library.enumeration.MapType;
+import nl.soccar.library.enumeration.Privilege;
+import nl.soccar.library.enumeration.TeamColour;
+import org.junit.Test;
+
+/**
+ * JUnit test that tests all nl.soccar.library.enumeration enumerations.
+ * 
+ * @author PTS34A
+ */
+public class EnumerationTest {
+    
+    /**
+     * Tests all enumerations
+     */
+    @Test
+    public void testAllEnumerations() {
+        BallType.valueOf(BallType.BOWLING.toString());
+        CarType.valueOf(CarType.CASUAL.toString());
+        Duration.valueOf(Duration.MINUTES_10.toString());
+        EventType.valueOf(EventType.ASSIST.toString());
+        GameStatus.valueOf(GameStatus.STARTED.toString());
+        MapType.valueOf(MapType.DESERT.toString());
+        Privilege.valueOf(Privilege.ADMINISTRATOR.toString());
+        TeamColour.valueOf(TeamColour.BLUE.toString());
+    }
+
+}
