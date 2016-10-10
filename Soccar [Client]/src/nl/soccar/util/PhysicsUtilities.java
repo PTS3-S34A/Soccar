c85272e168e3f93bddc191e4a72963651d24b2af
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2016-10-10 14:49:50 +0200

    Documented PhysicsUtilities

--- Soccar [Client]/src/nl/soccar/util/PhysicsUtilities.java
+++ Soccar [Client]/src/nl/soccar/util/PhysicsUtilities.java
@@ -4,47 +4,84 @@ import nl.soccar.ui.DisplayConstants;
 import nl.soccar.ui.physics.PhysicsContants;
 
 /**
- *
+ * Utility class which provides conversion methods between Physics and JavaFX
+ * pixels, and provides methods to calculate car sizes.
+ * 
  * @author PTS34A
  */
 public final class PhysicsUtilities {
 
-    // Convert a JBox2D x coordinate to a JavaFX pixel x coordinate
+    private PhysicsUtilities() {
+    }
+
+    /**
+     * Converts a JBox2D x-coordinate to a JavaFX pixel x-coordinate.
+     *
+     * @param x the JBox2D x-coodinate to convert.
+     * @return The JavaFX x-coordinate.
+     */
     public static float toPixelX(float x) {
         return x * PhysicsContants.PIXELS_PER_METER;
     }
 
-    // Convert a JBox2D y coordinate to a JavaFX pixel y coordinate
+    /**
+     * Converts a JBox2D y-coordinate to a JavaFX pixel y-coordinate
+     *
+     * @param y the JBox2D y-coordinate to convert.
+     * @return The JavaFX y-coordinate.
+     */
     public static float toPixelY(float y) {
         return DisplayConstants.MAP_HEIGHT - (y * PhysicsContants.PIXELS_PER_METER);
     }
 
-    // Convert a JBox2D width to pixel width
+    /**
+     * Converts a JBox2D width to a JavaFX pixel-width
+     *
+     * @param width the JBox2D width to convert.
+     * @return The JavaFX pixel-width.
+     */
     public static float toPixelWidth(float width) {
         return width * PhysicsContants.PIXELS_PER_METER;
     }
 
-    // Convert a JBox2D height to pixel height
+    /**
+     * Converts a JBox2D height to pixel-height
+     *
+     * @param height the JBox2D height to convert.
+     * @return The JavaFX pixel-height.
+     */
     public static float toPixelHeight(float height) {
         return height * PhysicsContants.PIXELS_PER_METER;
     }
-    
-    // Calculate relative car height based on giving width
+
+    /**
+     * Calculates relative car height based on given width.
+     * 
+     * @param carWidth The width to calculate the height from.
+     * @return The calculated height.
+     */
     public static float calculateCarHeight(float carWidth) {
         return carWidth * 47 / 20;
     }
-    
-    // Calculate relative wheel width based on given car width
+
+    /**
+     * Calculates relative wheel width based on given car width.
+     * 
+     * @param carWidth The width to calculate the wheel width from.
+     * @return The calculated wheel width.
+     */
     public static float calculateWheelWidth(float carWidth) {
         return carWidth / 5;
     }
-    
-    // Calculate relative wheel height based on hiven car width
+
+    /**
+     * Calculate relative wheel height based on given car width
+     * 
+     * @param wheelWidth The wheel width to calculate the wheel height from.
+     * @return The calculated wheel height.
+     */
     public static float calculateWheelHeight(float wheelWidth) {
         return wheelWidth * 2;
     }
-    
-    private PhysicsUtilities() {
-    }
-    
+
 }
