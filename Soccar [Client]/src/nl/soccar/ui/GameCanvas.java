a83a29dce6bba1ac627522b14ad13702116d75fa
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-10 11:29:42 +0200

    Added the getWorld method to the GameCanvas class.

--- Soccar [Client]/src/nl/soccar/ui/GameCanvas.java
+++ Soccar [Client]/src/nl/soccar/ui/GameCanvas.java
@@ -3,6 +3,8 @@ package nl.soccar.ui;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
 
 /**
  *
@@ -10,6 +12,15 @@ import java.util.List;
  */
 public abstract class GameCanvas {
 
+    private static final Vec2 GRAVITY;
+    private static final boolean  DO_SLEEP;
+    
+    static {
+        GRAVITY = new Vec2(0.0F, 0.0F);
+        DO_SLEEP = true;
+    }
+    
+    private final World world;
     private final List<Drawable> drawables;
 
     /**
@@ -18,6 +29,7 @@ public abstract class GameCanvas {
      */
     public GameCanvas() {
         drawables = new ArrayList<>();
+        world = new World(GRAVITY, DO_SLEEP);
     }
 
     public abstract void start();
@@ -40,6 +52,15 @@ public abstract class GameCanvas {
      */
     public final void removeDrawable(Drawable drawable) {
         drawables.remove(drawable);
+    }
+
+    /**
+     * Method that gets the world.
+     * 
+     * @return The world.
+     */
+    public World getWorld() {
+        return world;
     }
 
     /**
