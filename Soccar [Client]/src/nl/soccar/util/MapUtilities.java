d1571c806499af4ad58a6d1209955acce84c0c6a
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-11 20:02:39 +0200

    Changed the implementation of some utility methods in the PhysicsUtilities class and added methods to calculate the center X- and Y-coordinates of javafx.scene.shape Rectangles in the MapUtilities class.

--- Soccar [Client]/src/nl/soccar/util/MapUtilities.java
+++ Soccar [Client]/src/nl/soccar/util/MapUtilities.java
@@ -1,36 +1,57 @@
 package nl.soccar.util;
 
-import java.awt.Rectangle;
+import javafx.scene.shape.Rectangle;
 import nl.soccar.ui.DisplayConstants;
 
 /**
- *
+ * Class that containts static utility methods that are used for calculations on the map.
+ * 
  * @author PTS34A
  */
 public final class MapUtilities {
 
     /**
      * Method that calculates and returns the rectangle of the left goal of the map.
-     * 
+     *
      * @return Rectangle The rectangle of the the left goal of the map.
      */
     public static Rectangle getLeftGoal() {
-        double goalPositionY = (DisplayConstants.SCREEN_HEIGHT / 2) - (DisplayConstants.GOAL_HEIGHT / 2);
+        double goalPositionY = (DisplayConstants.MAP_HEIGHT / 2) + (DisplayConstants.GOAL_HEIGHT / 2);
 
-        return new Rectangle(DisplayConstants.FIELD_MARGIN - DisplayConstants.GOAL_WIDTH, (int) goalPositionY,
-                DisplayConstants.GOAL_WIDTH, (int) DisplayConstants.GOAL_HEIGHT);
+        return new Rectangle(DisplayConstants.FIELD_MARGIN - DisplayConstants.GOAL_WIDTH, goalPositionY,
+                DisplayConstants.GOAL_WIDTH, DisplayConstants.GOAL_HEIGHT);
     }
 
     /**
      * Method that calculates and returns the rectangle of the right goal of the map.
-     * 
+     *
      * @return Rectangle The rectangle of the right goal of the map.
      */
     public static Rectangle getRightGoal() {
-        double goalPositionY = (DisplayConstants.SCREEN_HEIGHT / 2) - (DisplayConstants.GOAL_HEIGHT / 2);
-        
-        return new Rectangle(DisplayConstants.SCREEN_WIDTH - DisplayConstants.FIELD_MARGIN, (int) goalPositionY,
+        double goalPositionY = (DisplayConstants.MAP_HEIGHT / 2) + (DisplayConstants.GOAL_HEIGHT / 2);
+
+        return new Rectangle(DisplayConstants.MAP_WIDTH - DisplayConstants.FIELD_MARGIN, (int) goalPositionY,
                 DisplayConstants.GOAL_WIDTH, (int) DisplayConstants.GOAL_HEIGHT);
+    }
+
+    /**
+     * Method that calculates the center X-coordinate of the given rectangle.
+     *
+     * @param rectangle The rectangle of which the center X-coordinate needs to be calculated.
+     * @return The center X-coordinate of the given rectangle.
+     */
+    public static double getCentreX(Rectangle rectangle) {
+        return rectangle.getX() + rectangle.getWidth() / 2;
+    }
+
+    /**
+     * Method that calculates the center Y-coordinate of the given rectangle.
+     *
+     * @param rectangle The rectangle of which the center Y-coordinate needs to be calculated.
+     * @return The center Y-coordinate of the given rectangle.
+     */
+    public static double getCentreY(Rectangle rectangle) {
+        return rectangle.getY() + rectangle.getHeight() / 2;
     }
 
     private MapUtilities() {
--- Soccar [Client]/src/nl/soccar/util/PhysicsUtilities.java
+++ Soccar [Client]/src/nl/soccar/util/PhysicsUtilities.java
@@ -4,47 +4,48 @@ import nl.soccar.ui.DisplayConstants;
 import nl.soccar.ui.physics.PhysicsContants;
 
 /**
- *
+ * Class that containts static utility methods that are used for physics calculations.
+ * 
  * @author PTS34A
  */
 public final class PhysicsUtilities {
 
-    // Convert a JBox2D x coordinate to a JavaFX pixel x coordinate
+    // Convert a JBox2D X-coordinate to a JavaFX pixel X-coordinate.
     public static float toPixelX(float x) {
         return x * PhysicsContants.PIXELS_PER_METER;
     }
 
-    // Convert a JBox2D y coordinate to a JavaFX pixel y coordinate
+    // Convert a JBox2D Y-coordinate to a JavaFX pixel Y-coordinate.
     public static float toPixelY(float y) {
-        return DisplayConstants.MAP_HEIGHT - (y * PhysicsContants.PIXELS_PER_METER);
+        return (DisplayConstants.MAP_HEIGHT - y) * PhysicsContants.PIXELS_PER_METER;
     }
 
-    // Convert a JBox2D width to pixel width
+    // Convert a JBox2D width to pixel width.
     public static float toPixelWidth(float width) {
         return width * PhysicsContants.PIXELS_PER_METER;
     }
 
-    // Convert a JBox2D height to pixel height
+    // Convert a JBox2D height to pixel height.
     public static float toPixelHeight(float height) {
         return height * PhysicsContants.PIXELS_PER_METER;
     }
-    
-    // Calculate relative car height based on giving width
+
+    // Calculate relative car height based on giving width.
     public static float calculateCarHeight(float carWidth) {
         return carWidth * 47 / 20;
     }
-    
-    // Calculate relative wheel width based on given car width
+
+    // Calculate relative wheel width based on given car width.
     public static float calculateWheelWidth(float carWidth) {
         return carWidth / 5;
     }
-    
-    // Calculate relative wheel height based on hiven car width
+
+    // Calculate relative wheel height based on hiven car width.
     public static float calculateWheelHeight(float wheelWidth) {
         return wheelWidth * 2;
     }
-    
+
     private PhysicsUtilities() {
     }
-    
+
 }
