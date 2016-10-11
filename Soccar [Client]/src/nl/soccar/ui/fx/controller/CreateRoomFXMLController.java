4dcf1e266b289b4a6668571c306d9c0cc9511ba7
bras1223 <l.hermans98@gmail.com>
2016-10-11 19:44:14 +0200

    Removed un-used library inputs.

--- Soccar [Client]/src/nl/soccar/ui/fx/controller/CreateRoomFXMLController.java
+++ Soccar [Client]/src/nl/soccar/ui/fx/controller/CreateRoomFXMLController.java
@@ -3,7 +3,6 @@ package nl.soccar.ui.fx.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
