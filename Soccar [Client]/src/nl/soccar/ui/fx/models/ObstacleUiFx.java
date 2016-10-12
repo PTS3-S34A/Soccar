62fa9cb7776d774e7db8560ac1eee20bf192262a
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-12 20:12:45 +0200

    Removed the implementatation of the draw method for walls because walls are invisible.

--- Soccar [Client]/src/nl/soccar/ui/fx/models/ObstacleUiFx.java
+++ Soccar [Client]/src/nl/soccar/ui/fx/models/ObstacleUiFx.java
@@ -3,15 +3,15 @@ package nl.soccar.ui.fx.models;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import nl.soccar.library.Obstacle;
+import nl.soccar.library.enumeration.ObstacleType;
 import nl.soccar.ui.fx.GameCanvasFx;
 import nl.soccar.ui.fx.PhysicsDrawableFx;
 import nl.soccar.ui.physics.models.ObstaclePhysics;
 import nl.soccar.util.PhysicsUtilities;
+import org.jbox2d.dynamics.World;
 
 /**
- * An ObstacleUiFx object represents a JavaFX Drawable of an Obstacle. It keeps track
- * of the Obstacle and ObstaclePhysics models and provides methods to draw and
- * update the models.
+ * An ObstacleUiFx object represents a JavaFX Drawable of an Obstacle. It keeps track of the Obstacle and ObstaclePhysics models and provides methods to draw and update the models.
  *
  * @author PTS34A
  */
@@ -30,7 +30,7 @@ public class ObstacleUiFx extends PhysicsDrawableFx<Obstacle, ObstaclePhysics> {
      * @param obstacle The model to keep track of.
      * @param physics The physics-model to keep track of.
      */
-    public ObstacleUiFx(GameCanvasFx canvas, Obstacle obstacle, ObstaclePhysics physics) {
+    private ObstacleUiFx(GameCanvasFx canvas, Obstacle obstacle, ObstaclePhysics physics) {
         super(canvas, obstacle, physics);
     }
 
@@ -52,16 +52,70 @@ public class ObstacleUiFx extends PhysicsDrawableFx<Obstacle, ObstaclePhysics> {
 
         context.translate(x, y); // Set the origin point of the rotation.
         context.rotate(-obstacle.getDegree()); // Set the angle of the rotation.
-        
-        switch (obstacle.getType()) {            
+
+        switch (obstacle.getType()) {
             default:
             case WALL:
-                // TODO: remove drawing implementation of the wall after testing.
-                context.setFill(WALL_COLOR);
-                context.fillRect(-width / 2, -height / 2, width, height); // Draw the rectangle from the top left
+                // The wall obstacle is never drawn, it is invisible.
                 break;
         }
 
         context.restore(); // Restore canvas to display a rotated image.
+    }
+
+    public static class ObstacleBuilder {
+
+        private final GameCanvasFx canvas;
+        private final World world;
+
+        private float x;
+        private float y;
+        private float degree;
+        private float width;
+        private float height;
+        private ObstacleType type;
+
+        public ObstacleBuilder(GameCanvasFx canvas, World world) {
+            this.canvas = canvas;
+            this.world = world;
+        }
+
+        public ObstacleBuilder x(float x) {
+            this.x = x;
+            return this;
+        }
+
+        public ObstacleBuilder y(float y) {
+            this.y = y;
+            return this;
+        }
+
+        public ObstacleBuilder degree(float degree) {
+            this.degree = degree;
+            return this;
+        }
+
+        public ObstacleBuilder width(float width) {
+            this.width = width;
+            return this;
+        }
+
+        public ObstacleBuilder height(float height) {
+            this.height = height;
+            return this;
+        }
+
+        public ObstacleBuilder type(ObstacleType type) {
+            this.type = type;
+            return this;
+        }
+
+        public ObstacleUiFx build() {
+            Obstacle obstacle = new Obstacle(x, y, degree, width, height, type);
+            ObstaclePhysics obstaclePhysics = new ObstaclePhysics(obstacle, world);
+            ObstacleUiFx obstacleUiFx = new ObstacleUiFx(canvas, obstacle, obstaclePhysics);
+            return obstacleUiFx;
+        }
+        
     }
 }
