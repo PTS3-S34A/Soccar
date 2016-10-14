a95be2d8695c3b6edaf73801afedc0f66840f710
Marc <mjgroos@live.nl>
2016-10-14 12:12:41 +0200

    Added action methods to Keyboard class and added handbrake support.

--- Soccar [Client]/src/nl/soccar/ui/input/Keyboard.java
+++ Soccar [Client]/src/nl/soccar/ui/input/Keyboard.java
@@ -1,22 +1,38 @@
 package nl.soccar.ui.input;
 
-import java.util.ArrayList;
-import java.util.List;
 import javafx.scene.input.KeyCode;
+import nl.soccar.ui.physics.enumeration.HandbrakeAction;
+import nl.soccar.ui.physics.enumeration.SteerAction;
+import nl.soccar.ui.physics.enumeration.ThrottleAction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * The Keyboard class keeps track of all pressed (and in turn released) keys.
- * 
+ *
  * @author PTS34A
  */
 public final class Keyboard {
 
-    private static final Keyboard INSTANCE = new Keyboard();
+    private static List<KeyCode> PRESSED_KEYS;
 
-    private List<KeyCode> pressedKeys;
+    private static final KeyCode[] ACCELERATE;
+    private static final KeyCode[] REVERSE;
+    private static final KeyCode[] STEER_LEFT;
+    private static final KeyCode[] STEER_RIGHT;
+    private static final KeyCode[] HANDBRAKE;
 
-    private Keyboard() {
-        pressedKeys = new ArrayList<>();
+
+    static {
+        PRESSED_KEYS = new ArrayList<>();
+
+        ACCELERATE = new KeyCode[]{KeyCode.W, KeyCode.UP};
+        REVERSE = new KeyCode[]{KeyCode.S, KeyCode.DOWN};
+        STEER_LEFT = new KeyCode[]{KeyCode.A, KeyCode.LEFT};
+        STEER_RIGHT = new KeyCode[]{KeyCode.D, KeyCode.RIGHT};
+        HANDBRAKE = new KeyCode[]{KeyCode.SPACE};
     }
 
     /**
@@ -27,8 +43,8 @@ public final class Keyboard {
      * @return boolean True if already pressed, false if not in pressedKeys
      * list.
      */
-    public boolean isPressed(KeyCode code) {
-        return pressedKeys.contains(code);
+    public static boolean isPressed(KeyCode code) {
+        return PRESSED_KEYS.contains(code);
     }
 
     /**
@@ -36,9 +52,9 @@ public final class Keyboard {
      *
      * @param code The keycode that needs to be added to the pressedKeys list.
      */
-    public void setKeyPressed(KeyCode code) {
-        if (!pressedKeys.contains(code)) {
-            pressedKeys.add(code);
+    public static void setKeyPressed(KeyCode code) {
+        if (!PRESSED_KEYS.contains(code)) {
+            PRESSED_KEYS.add(0, code); // Prepend to the list, so the last key pressed gets first priority
         }
     }
 
@@ -47,17 +63,74 @@ public final class Keyboard {
      *
      * @param code The keycode that needs to be removed of the pressedKeys list.
      */
-    public void setKeyReleased(KeyCode code) {
-        pressedKeys.remove(code);
+    public static void setKeyReleased(KeyCode code) {
+        PRESSED_KEYS.remove(code);
+    }
+
+
+    /**
+     * Returns the correct ThrottleAction based on the current pressed keys
+     *
+     * @return ThrottleAction
+     */
+    public static ThrottleAction getThrottleAction() {
+
+        // The last pressed key
+        for (KeyCode pressedKey : PRESSED_KEYS) {
+
+            if (Arrays.asList(ACCELERATE).contains(pressedKey)) {
+                return ThrottleAction.ACCELERATE;
+            }
+
+            if (Arrays.asList(REVERSE).contains(pressedKey)) {
+                return ThrottleAction.REVERSE;
+            }
+
+        }
+
+        return ThrottleAction.IDLE;
     }
 
     /**
-     * Method that gets this static instance of the Keyboard object.
+     * Returns the correct SteerAction based on the current pressed keys
      *
-     * @return This keyboard object.
+     * @return SteerAction
      */
-    public static Keyboard getInstance() {
-        return INSTANCE;
+    public static SteerAction getSteerAction() {
+
+        for (KeyCode pressedKey : PRESSED_KEYS) {
+
+            if (Arrays.asList(STEER_LEFT).contains(pressedKey)) {
+                return SteerAction.STEER_LEFT;
+            }
+
+            if (Arrays.asList(STEER_RIGHT).contains(pressedKey)) {
+                return SteerAction.STEER_RIGHT;
+            }
+
+        }
+
+        return SteerAction.NONE;
     }
 
+    /**
+     * Returns the correct HandbrakeAction based on the current pressed keys
+     *
+     * @return SteerAction
+     */
+    public static HandbrakeAction getHandbrakeAction() {
+
+        for (KeyCode pressedKey : PRESSED_KEYS) {
+
+            if (Arrays.asList(HANDBRAKE).contains(pressedKey)) {
+                return HandbrakeAction.ACTIVE;
+            }
+
+        }
+
+        return HandbrakeAction.INACTIVE;
+    }
+
+    private Keyboard() {
+    }
 }
