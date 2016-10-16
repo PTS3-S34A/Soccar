b282522e8fdd491777fadf5b238de341733d2bae
Robin Laugs <Laugslander@users.noreply.github.com>
2016-10-16 18:25:10 +0200

    Fixed the requested changed. Removed the velocity field from the Entity class.

--- Soccar [Client]/src/nl/soccar/library/Entity.java
+++ Soccar [Client]/src/nl/soccar/library/Entity.java
@@ -11,7 +11,6 @@ public abstract class Entity {
     protected float x;
     protected float y;
     protected float degree;
-    protected float velocity;
 
     /**
      * Initiates a new Entity Object.
@@ -24,7 +23,6 @@ public abstract class Entity {
         this.x = x;
         this.y = y;
         this.degree = degree;
-        velocity = 0.0F;
     }
     
     /**
