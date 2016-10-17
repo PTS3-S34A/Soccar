81894b1789f299d05f138b96e8a6e5ddce6d52f3
bras1223 <l.hermans98@gmail.com>
2016-10-17 16:14:00 +0200

    Fixed new line.

--- Soccar [Client]/src/nl/soccar/util/ImageUtilities.java
+++ Soccar [Client]/src/nl/soccar/util/ImageUtilities.java
@@ -6,20 +6,22 @@ import nl.soccar.library.enumeration.TeamColour;
 
 /**
  * Utility class that provides methods regarding used images.
- * 
+ *
  * @author PTS34A
  */
 public final class ImageUtilities {
-    
+
     private ImageUtilities() {
         // Private method to prevent instantiation.
     }
-    
+
     /**
      * Converts a CarType and TeamColour to an Image.
-     * 
-     * @param carType The CarType of the image. This will be used to determine what file the image should be loaded from.
-     * @param teamColour The TeamColour of the image. This will be used to determine what file the image should be loaded from.
+     *
+     * @param carType The CarType of the image. This will be used to determine
+     * what file the image should be loaded from.
+     * @param teamColour The TeamColour of the image. This will be used to
+     * determine what file the image should be loaded from.
      * @return An Image containing the correct imagesource.
      */
     public static Image getCarImage(CarType carType, TeamColour teamColour) {
@@ -28,8 +30,8 @@ public final class ImageUtilities {
         builder.append("_");
         builder.append(teamColour.name().toLowerCase());
         builder.append("_texture.png");
-        
+
         return new Image(builder.toString());
     }
-    
+
 }
