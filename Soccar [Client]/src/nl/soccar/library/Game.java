a1ccdeaee1749d3bb40cf1a77015c60bea5611c4
Marc Groos <mjgroos@live.nl>
2016-10-17 22:37:00 +0200

    Added lastBallTouched and modified GameStatus.

--- Soccar [Client]/src/nl/soccar/library/Game.java
+++ Soccar [Client]/src/nl/soccar/library/Game.java
@@ -1,17 +1,18 @@
 package nl.soccar.library;
 
+import nl.soccar.library.enumeration.BallType;
+import nl.soccar.library.enumeration.Duration;
+import nl.soccar.library.enumeration.GameStatus;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import nl.soccar.library.enumeration.BallType;
-import nl.soccar.library.enumeration.Duration;
-import nl.soccar.library.enumeration.GameStatus;
 
 /**
  * A Game contains all kind of information about a Match.
- * 
+ *
  * @author PTS34A
  */
 public class Game {
@@ -20,9 +21,10 @@ public class Game {
     private Duration duration;
     private BallType balltype;
     private GameStatus status;
+    private Player lastBallTouched;
 
     private List<Event> events;
-    
+
     /**
      * Initiates a new Game Object.
      */
@@ -36,30 +38,28 @@ public class Game {
      * Starts this Game.
      */
     public void start() {
-        // TODO implementatie
-        throw new UnsupportedOperationException("Not supported yet.");
+        status = GameStatus.RUNNING;
     }
 
     /**
      * Ends this Game.
      */
     public void stop() {
-        // TODO implementatie
-        throw new UnsupportedOperationException("Not supported yet.");
+        status = GameStatus.STOPPED;
     }
 
     /**
      * Adds an Event to this Game.
-     * 
+     *
      * @param event The Event that occurred in this Game.
      */
     public void addEvent(Event event) {
         events.add(event);
     }
-    
+
     /**
      * Gets all Events that occurred in this Game.
-     * 
+     *
      * @return A List of Events that occurred in this Game.
      */
     public List<Event> getEvents() {
@@ -68,7 +68,7 @@ public class Game {
 
     /**
      * Gets the time at which this Game started, if there's no current match on going, the Optional will be empty.
-     * 
+     *
      * @return An Optional that contains the start time of the current match.
      */
     public Optional<LocalDateTime> getStartTime() {
@@ -77,7 +77,7 @@ public class Game {
 
     /**
      * Gets the status of this Game.
-     * 
+     *
      * @return The status of this Game.
      */
     public GameStatus getStatus() {
@@ -85,8 +85,17 @@ public class Game {
     }
 
     /**
+     * Sets the status of this Game.
+     *
+     * @param status The status to be set.
+     */
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
+    /**
      * Gets the Duration of this Game.
-     * 
+     *
      * @return The Duration of this Game.
      */
     public Duration getDuration() {
@@ -95,7 +104,7 @@ public class Game {
 
     /**
      * Sets the Duration of this Game.
-     * 
+     *
      * @param duration The new Duration of this Game.
      */
     public void setDuration(Duration duration) {
@@ -104,7 +113,7 @@ public class Game {
 
     /**
      * Gets the BallType that should be used for this Game.
-     * 
+     *
      * @return The BallType that should be used for this Game.
      */
     public BallType getBalltype() {
@@ -113,11 +122,28 @@ public class Game {
 
     /**
      * Sets the new BallType that should be used for this Game.
-     * 
+     *
      * @param balltype The new BallType that should be used for this Game.
      */
     public void setBalltype(BallType balltype) {
         this.balltype = balltype;
     }
 
+    /**
+     * Gets the last player who touched the ball.
+     *
+     * @return
+     */
+    public Player getLastBallTouched() {
+        return lastBallTouched;
+    }
+
+    /**
+     * Sets the last player who touched the ball.
+     *
+     * @param lastBallTouched
+     */
+    public void setLastBallTouched(Player lastBallTouched) {
+        this.lastBallTouched = lastBallTouched;
+    }
 }
