7131829539924e23085eee95c79725d0d23b7f45
Marc Groos <mjgroos@live.nl>
2016-10-17 22:49:32 +0200

    Added goal scoring logic.

--- Soccar [Client]/src/nl/soccar/ui/physics/models/BallPhysics.java
+++ Soccar [Client]/src/nl/soccar/ui/physics/models/BallPhysics.java
@@ -1,9 +1,14 @@
 package nl.soccar.ui.physics.models;
 
-import nl.soccar.library.Ball;
+import nl.soccar.library.*;
+import nl.soccar.library.enumeration.EventType;
+import nl.soccar.library.enumeration.GameStatus;
 import nl.soccar.ui.physics.WorldObject;
 import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.*;
+
+import java.time.LocalDateTime;
 
 /**
  * BallPhysics is a physics-model that keeps track of the physics of the Ball.
@@ -19,8 +24,11 @@ public class BallPhysics implements WorldObject {
     private static final float LINEAR_DAMPING = 1.0F;
     private static final float ANGULAR_DAMPING = 1.0F;
 
+    private final Vec2 originalPos;
+
     private final Body body;
     private final float radius;
+    private Ball ball;
 
     /**
      * Initiates a new BallPhysics Object using the given parameter.
@@ -29,11 +37,13 @@ public class BallPhysics implements WorldObject {
      * @param world The world in which this model is placed.
      */
     public BallPhysics(Ball ball, World world) {
+        this.ball = ball;
+        originalPos = new Vec2(ball.getX(), ball.getY());
         radius = ball.getRadius();
 
         BodyDef bd = new BodyDef();
         bd.type = BodyType.DYNAMIC;
-        bd.position.set(ball.getX(), ball.getY());
+        bd.position.set(originalPos);
         bd.linearDamping = LINEAR_DAMPING;
         bd.angularDamping = ANGULAR_DAMPING;
 
@@ -45,6 +55,7 @@ public class BallPhysics implements WorldObject {
         fd.friction = FRICTION;
         fd.restitution = RESTITUTION;
         fd.shape = cs;
+        fd.userData = ball;
 
         body = world.createBody(bd);
         body.createFixture(fd);
@@ -52,7 +63,38 @@ public class BallPhysics implements WorldObject {
 
     @Override
     public void step() {
-        // The step method is not implemented because Box2D handles all necessary updates.
+        ball.move(getX(), getY(), getDegree());
+        checkGoalScored();
+    }
+
+    @Override
+    public void reset() {
+        body.setLinearVelocity(new Vec2(0.0F, 0.0F));
+        body.setAngularVelocity(0.0F);
+        body.setTransform(originalPos, 0.0F);
+    }
+
+    /**
+     * Checks if the ball passes by the goal line.
+     * When it does, it adds an event to the Game object and sets the GameStatus to SCORED.
+     */
+    public void checkGoalScored() {
+
+        Game game = Soccar.getInstance().getSessionController().getCurrentSession().getGame();
+        Player player = Soccar.getInstance().getCurrentPlayer();
+
+        float ballX = ball.getX();
+
+        if (ballX > 156.0F) {
+            game.addEvent(new Event(EventType.GOAL_RED, LocalDateTime.now(), player));
+            game.setStatus(GameStatus.SCORED);
+            System.out.println("GOAL RED");
+        } else if (ballX < 4.0F) {
+            game.addEvent(new Event(EventType.GOAL_BLUE, LocalDateTime.now(), player));
+            game.setStatus(GameStatus.SCORED);
+            System.out.println("GOAL BLUE");
+        }
+
     }
 
     @Override
